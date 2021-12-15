52ce875d96c2c748848cff8e4551189067092c43
kyle65463 <kyle65463@gmail.com>
2021-12-15 16:30:33 +0800

    Add daisy UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,9 @@ function Home() {
 				<title>Title</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main className='text-5xl bg-red-200'>HELLO WORLD</main>
+			<main className='container mx-auto my-5'>
+				<h1 className='text-5xl bg-red-200 p-5'>HELLO WORLD</h1>
+			</main>
 		</div>
 	);
 }
